ecdfc6b3f015836f13a939c32c4869c76aca43c8
dipanshuupashyay123 <dipanshuupadhyay45@gmail.com>
2026-08-13 12:27:14 +0530

    disable login for deployment

--- dashboard/src/components/ProtectedRoute.jsx
+++ dashboard/src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
+  // const token = localStorage.getItem("token");
 
-  return token ? children : <Navigate to="/login" />;
+  // return token ? children : <Navigate to="/login" />;
+  return children;
 };
 
 export default ProtectedRoute;
